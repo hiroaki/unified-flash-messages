cbbf67c95dfdb075095542580a428a6729034eb7
WATANABE Hiroaki <176736+hiroaki@users.noreply.github.com>
2025-10-11 16:48:17 +0900

    setup javascript

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import { initializeFlashMessageSystem } from "flash_messages"
-
-if (document.readyState === "loading") {
-  document.addEventListener("DOMContentLoaded", initializeFlashMessageSystem);
-} else {
-  initializeFlashMessageSystem();
-}
+import "flash_unified/auto";
